be50fb1a8d77cd34cf484eac0a670075bd785e14
Huynh Hoang Phuc <phuchh@bap.jp>
2019-08-14 22:32:59 +0700

    Updates

---
+++ precache-manifest.201376d04ac198078df81e02e9fc5fbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdbae41e8d52bb58e43b96ca2733c03",
+    "url": "/kanji-pro/index.html"
+  },
+  {
+    "revision": "92a7a8a9e9f7d6a7e4dc",
+    "url": "/kanji-pro/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4a74241a820dac6df65e",
+    "url": "/kanji-pro/static/js/2.511b5457.chunk.js"
+  },
+  {
+    "revision": "92a7a8a9e9f7d6a7e4dc",
+    "url": "/kanji-pro/static/js/main.31795460.chunk.js"
+  },
+  {
+    "revision": "1ffb721212b25b8f224d",
+    "url": "/kanji-pro/static/js/runtime~main.c00cc48e.js"
+  }
+]);
